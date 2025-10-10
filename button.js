f75c7867362a8f147cf2762faee5703a089e03e7
Ljuery0 <ljuery0@gmail.com>
2025-10-10 13:25:19 +0200

    Add files via upload

---
+++ button.js
@@ -0,0 +1,62 @@
+function calcul_moyenne(){
+    var n1 = prompt("Donner la prémière note :");
+    var n2 = prompt("Donner la deuxième note :");
+    var n3 = prompt("Donner la troisième note :");
+    
+    var somme = Number(n1) + Number(n2) + Number(n3);
+    document.write("Voici la somme :" + somme + "<br>");
+    
+    var moyenne = somme/3;
+    document.write("Voici la moyenne: " + moyenne +"<br>");
+
+    if (moyenne < 10 ){
+        document.write("Vous n'êtes pas admis");
+    } 
+    else {
+        document.write("Vous êtes admis");
+    }
+}
+
+function test_age() {
+    let age = prompt("Quel est votre âge?");
+    if (age < 18) {
+        document.write("Vous êtes mineur!");
+        document.bgColor = "red"
+    }
+    else {
+        document.write("Vous êtes majeur!");
+        document.bgColor = "green"
+    }
+}
+
+function simple_affichage() {
+    let name = prompt("Donner votre nom:");
+    let prenom = prompt("Donner votre prénom:");
+    
+    document.write('<div style="margin:auto; width:300px; border:2px solid blue;">');
+    document.write("Bonjour" + prenom + " " + name);
+    document.write('</div>');
+}
+
+function test_couleur() {
+    let couleur = prompt("Donner une couleur:");
+    if (couleur == "rouge" || couleur == "red") {
+        document.body.style.backgroundColor = "red"
+    }
+
+    else if (couleur == "bleu" || couleur == "blue") {
+         document.body.style.backgroundColor = "blue"
+    }
+    else if (couleur == "vert" || couleur == "green") {
+         document.body.style.backgroundColor = "green"
+    }
+     else if (couleur == "yellow" || couleur == "yellow") {
+         document.body.style.backgroundColor = "yellow"
+    }
+     else if (couleur == "blanc" || couleur == "white") {
+         document.body.style.backgroundColor = "white"
+    }
+     else {
+         document.write("Désolé, couleur non comprise")
+    }
+}
